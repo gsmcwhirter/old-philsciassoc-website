20fbc451b3c05c8609a314e277ef15499da078cf
Gregory McWhirter <greg@hallofkvasir.org>
2009-12-10 19:00:22 -0800

    Final changes

--- philsci_testing.sh
+++ philsci_testing.sh
@@ -8,9 +8,9 @@ cd ~/sites/PhilSci
 git checkout generator/generator.php
 git checkout remote_deploy.sh
 git pull
-perl -pi -w -e 's!^basepath=~/public_html/generator!basepath=~/domains/philsci.org/web/public!g' remote_deploy.sh
-perl -pi -w -e 's!^targetpath=\$basepath/target!targetpath=$basepath/staging!g' remote_deploy.sh
-perl -pi -w -e 's!^sourcepath=~/sites/PhilSci!sourcepath=~/dev/PhilSci!g' remote_deploy.sh
+perl -pi -w -e 's!^basepath=~/public_html/generator!basepath=\$HOME/domains/philsci.org/web/public!g' remote_deploy.sh
+perl -pi -w -e 's!^targetpath=\$basepath/target!targetpath=\$basepath/staging!g' remote_deploy.sh
+perl -pi -w -e 's!^sourcepath=~/sites/PhilSci!sourcepath=\$HOME/dev/PhilSci!g' remote_deploy.sh
 ./remote_deploy.sh
 perl -pi -w -e 's!^define\("GENERATOR_ENVIRONMENT","local"\)!define\("GENERATOR_ENVIRONMENT","development"\)!g' generator/generator.php
 /usr/bin/env php generator/generator.php testing
